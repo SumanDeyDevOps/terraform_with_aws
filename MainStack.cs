3dd321992b7153c80b5170ef7adde94ff450615f
sumandey.nci <sumandey.nci@gmail.com>
2024-11-24 22:37:42 +0000

    Initial commit

---
+++ MainStack.cs
@@ -0,0 +1,28 @@
+using System;
+using Constructs;
+using HashiCorp.Cdktf;
+
+using HashiCorp.Cdktf.Providers.Aws.Provider;
+using HashiCorp.Cdktf.Providers.Aws.Instance;
+
+namespace MyCompany.MyApp
+{
+    class MainStack : TerraformStack
+    {
+        public MainStack(Construct scope, string id) : base(scope, id)
+        {
+            new AwsProvider(this, "AWS", new AwsProviderConfig { Region = "eu-west-1" });
+
+            Instance instance = new Instance(this, "compute", new InstanceConfig
+            {
+                Ami = "ami-02141377eee7defb9",
+                InstanceType = "t2.micro",
+            });
+
+            new TerraformOutput(this, "public_ip", new TerraformOutputConfig
+            {
+                Value = instance.PublicIp
+            });
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+using System;
+using Constructs;
+using HashiCorp.Cdktf;
+
+namespace MyCompany.MyApp
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            App app = new App();
+            MainStack stack = new MainStack(app, "aws_instance");
+            new RemoteBackend(
+                stack,
+                new RemoteBackendProps
+                {
+                    Hostname = "app.terraform.io",
+                    Organization = "Athlone_Institute",
+                    Workspaces = new NamedRemoteWorkspace("StackUsingTerraform")
+                }
+            );
+
+            app.Synth();
+        }
+    }
+}
